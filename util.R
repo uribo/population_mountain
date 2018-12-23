eb35f6f301661899cd419a6ba614e2878e31066b
Shinya Uryu <suika1127@gmail.com>
2018-12-23 10:47:16 +0900

    2015以前のファイルに対応

--- util.R
+++ util.R
@@ -7,21 +7,43 @@ pop_mesh <- function(file) {
   
   meshcode <- geometry <- NULL
   
-  readr::read_csv(file = file, 
-                  skip = 2,
-                  col_names = c("meshcode", "hitoku_syori",
-                                paste("population", 
-                                      c("total", "man", "woman", "age65gt"), 
-                                      sep = "_")),
-                  col_types = paste0("cc__ddd", 
-                                     paste(rep("_", 12), collapse = ""),
-                                     "d", 
-                                     paste(rep("_", 25), collapse = "")),
-                  locale = readr::locale(encoding = "cp932")) %>%
-    dplyr::mutate(geometry = purrr::map_chr(meshcode, 
-                                            ~ jpmesh::export_mesh(.x) %>% 
-                                            sf::st_as_text())) %>% 
-    dplyr::mutate(geometry = sf::st_as_sfc(geometry)) %>% 
+  target_vars <- 
+    c("meshcode",
+      paste("population", 
+            c("total", "man", "woman"),
+            sep = "_"),
+      "setai")
+  
+  
+  checked <- 
+    check_yr(file)
+  
+  if (checked == "before2015") {
+    df <- 
+      readr::read_csv(file = file, 
+                    skip = 2,
+                    col_names = target_vars,
+                    col_types = "cdddd",
+                    locale = readr::locale(encoding = "cp932"))
+  }
+
+  if (checked == "after2015") {
+    df <- 
+      readr::read_csv(file = file, 
+                    skip = 2,
+                    col_names = target_vars,
+                    col_types = paste0("c___ddd", # 7 
+                                       paste(rep("_", 21), collapse = ""),
+                                       "d", # 29
+                                       paste(rep("_", 16), collapse = "")),
+                    locale = readr::locale(encoding = "cp932"))  
+  }
+  
+  df %>% 
+    dplyr::mutate(geometry = purrr::map_chr(meshcode,
+                                            ~ jpmesh::export_mesh(.x) %>%
+                                            sf::st_as_text())) %>%
+    dplyr::mutate(geometry = sf::st_as_sfc(geometry)) %>%
     sf::st_sf(crs = 4326)
 }
 
@@ -42,4 +64,17 @@ var_uncount <- function(data, bar_var, scale = 100) {
     sf::st_set_geometry(NULL) %>% 
     tidyr::uncount(var) %>% 
     tibble::as_tibble()
+}
+
+check_yr <- function(file) {
+  
+  checked <- 
+    readr::read_lines(file, n_max = 1) %>% 
+    stringr::str_count(",")
+  
+  ifelse(
+    checked == 4L,
+    "before2015",
+    "after2015"
+  )
 }
